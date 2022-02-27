4168e236f97347f87348ee45d69007bfea0b5c4c
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2022-02-27 12:23:43 +0530

    Upgraded dependencies

--- test/setup.sh
+++ test/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf /tmp/betro-server &&
-git clone https://prijindal@github.com/betro-app/betro-server.git /tmp/betro-server -b master --depth 1 &&
+git clone https://prijindal@github.com/prijindal/betro-server.git /tmp/betro-server -b master --depth 1 &&
 cd /tmp/betro-server &&
 npm install &&
 npm run build
